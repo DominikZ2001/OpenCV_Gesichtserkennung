6a926d993ebc6aca081baf6b3ab6f90568c0f35c
DominikZ2001 <dominik.zunker@schuetz.net>
2021-11-25 13:06:59 +0100

    Import von face_recognition und numpy

--- Gesichtserkennung.py
+++ Gesichtserkennung.py
@@ -1,15 +1,7 @@
 import cv2
 import numpy as np
+import face_recognition
 
-cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
-cap.set(10,100)
 
-while True:
-    success, img = cap.read()
-    cv2.imshow("Video", img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break 
 
 
